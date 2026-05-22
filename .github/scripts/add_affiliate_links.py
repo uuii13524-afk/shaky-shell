d087c87ab320ee8a9415cf90adf86d02de0ebc66
Acia <fanshengshangmai24@gmail.com>
2026-05-22 09:46:22 +0900

    update affiliate links with new domain links

--- .github/scripts/add_affiliate_links.py
+++ .github/scripts/add_affiliate_links.py
@@ -1,40 +1,64 @@
 import os
-import re
 
-# アフィリエイトリンク定義
+# VPS系リンク
 CONOHA = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3UZB+CFPZOY+50+4Z0M6A" rel="nofollow">ConoHa VPS</a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=4B3UZB+CFPZOY+50+4Z0M6A" alt="">'
-XSERVER = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3UZC+2G46B6+CO4+25EKCY" rel="nofollow">XServer VPS</a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=4B3UZC+2G46B6+CO4+25EKCY" alt="">'
+XSERVER_VPS = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3UZC+2G46B6+CO4+25EKCY" rel="nofollow">XServer VPS</a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=4B3UZC+2G46B6+CO4+25EKCY" alt="">'
 SAKURA = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3UZB+GC8AGI+D8Y+BWVTE" rel="nofollow">さくらのVPS</a><img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=4B3UZB+GC8AGI+D8Y+BWVTE" alt="">'
 GMO = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3VRB+6S3QSY+2KX0+1HL85U" rel="nofollow">GMOクラウド ALTUS</a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=4B3VRB+6S3QSY+2KX0+1HL85U" alt="">'
+
+# ドメイン系リンク
 ONAMAE = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3VRB+64VU7M+50+2HHVNM" rel="nofollow">お名前.com</a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=4B3VRB+64VU7M+50+2HHVNM" alt="">'
 MUUMUU = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3VRB+69NB1U+348+1BNBJM" rel="nofollow">ムームードメイン</a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=4B3VRB+69NB1U+348+1BNBJM" alt="">'
+VALUE_DOMAIN = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3WJ5+B72HBM+1JUK+I3D2Q" rel="nofollow">Value-Domain</a><img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=4B3WJ5+B72HBM+1JUK+I3D2Q" alt="">'
+XSERVER_DOMAIN = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3WJ5+BFEJSI+CO4+15ORS2" rel="nofollow">XServerドメイン</a><img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=4B3WJ5+BFEJSI+CO4+15ORS2" alt="">'
+STAR_DOMAIN = '<a href="https://px.a8.net/svt/ejp?a8mat=4B3WJ5+BDM8Z6+1WTI+C0B9U" rel="nofollow">スタードメイン</a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=4B3WJ5+BDM8Z6+1WTI+C0B9U" alt="">'
 
-# 記事カテゴリ別リンクマッピング
-LINK_MAP = {
-    # VPS・サーバー・Docker・Linux・nginx系
-    'docker':  f'\n\n## おすすめのVPS\n\nDockerを本番環境で動かすなら、VPSが必要です。\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n- {GMO}\n',
-    'linux':   f'\n\n## おすすめのVPS\n\nLinuxを本番環境で使うなら、VPSが手軽です。\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n- {GMO}\n',
-    'nginx':   f'\n\n## おすすめのVPS\n\nnginxを動かすなら、VPSが必要です。\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n- {GMO}\n',
-    'vps':     f'\n\n## おすすめのVPS\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n- {GMO}\n',
-    'ssh':     f'\n\n## おすすめのVPS\n\nSSHで接続できるVPSをお探しなら。\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n- {GMO}\n',
-    # Cloudflare・DNS・ドメイン系
-    'cloudflare': f'\n\n## ドメイン取得はこちら\n\nCloudflareと組み合わせるドメインの取得に。\n\n- {ONAMAE}\n- {MUUMUU}\n',
-    'xserver':    f'\n\n## ドメイン取得はこちら\n\n- {ONAMAE}\n- {MUUMUU}\n',
-    'domain':     f'\n\n## ドメイン取得はこちら\n\n- {ONAMAE}\n- {MUUMUU}\n',
-    # GitHub Actions・git系
-    'github-actions': f'\n\n## おすすめのVPS\n\nGitHub Actionsで自動デプロイするサーバーをお探しなら。\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n',
-    # その他（デフォルト）
-    'default': f'\n\n## おすすめのVPS\n\n- {CONOHA}\n- {XSERVER}\n- {SAKURA}\n',
-}
+VPS_BLOCK = f'''
 
-def get_affiliate_block(filename):
+## おすすめのVPS
+
+VPSを使って本番環境を構築するなら、以下のサービスがおすすめです。
+
+- {CONOHA}
+- {XSERVER_VPS}
+- {SAKURA}
+- {GMO}
+'''
+
+DOMAIN_BLOCK = f'''
+
+## ドメイン取得はこちら
+
+Cloudflareと組み合わせるドメインの取得に。
+
+- {ONAMAE}
+- {MUUMUU}
+- {VALUE_DOMAIN}
+- {XSERVER_DOMAIN}
+- {STAR_DOMAIN}
+'''
+
+DEFAULT_BLOCK = f'''
+
+## おすすめのVPS
+
+- {CONOHA}
+- {XSERVER_VPS}
+- {SAKURA}
+'''
+
+VPS_KEYWORDS = ['docker', 'linux', 'nginx', 'vps', 'ssh', 'wsl', 'github-actions']
+DOMAIN_KEYWORDS = ['cloudflare', 'xserver', 'domain', 'astro-cloudflare', 'astro-sitemap']
+
+def get_block(filename):
     slug = filename.replace('.md', '')
-    for key in LINK_MAP:
-        if key == 'default':
-            continue
-        if key in slug:
-            return LINK_MAP[key]
-    return LINK_MAP['default']
+    for kw in DOMAIN_KEYWORDS:
+        if kw in slug:
+            return DOMAIN_BLOCK
+    for kw in VPS_KEYWORDS:
+        if kw in slug:
+            return VPS_BLOCK
+    return DEFAULT_BLOCK
 
 posts_dir = 'src/pages/posts'
 updated = 0
@@ -43,18 +67,20 @@ skipped = 0
 for filename in os.listdir(posts_dir):
     if not filename.endswith('.md'):
         continue
-
     filepath = os.path.join(posts_dir, filename)
     with open(filepath, 'r', encoding='utf-8') as f:
         content = f.read()
 
-    # すでにa8.netリンクが入っている場合はスキップ
-    if 'a8.net' in content:
-        skipped += 1
-        continue
+    # 既存のアフィリエイトブロックを削除して入れ直す
+    if '## おすすめのVPS' in content or 'a8.net' in content:
+        # ブロック前のコンテンツだけ残す
+        for marker in ['## おすすめのVPS', '## ドメイン取得はこちら']:
+            if marker in content:
+                content = content[:content.index(marker)].rstrip()
+                break
 
-    affiliate_block = get_affiliate_block(filename)
-    new_content = content.rstrip() + affiliate_block
+    block = get_block(filename)
+    new_content = content + block
 
     with open(filepath, 'w', encoding='utf-8') as f:
         f.write(new_content)
